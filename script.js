3c537811a9c9548f077d56e796846f311b662d9e
Leonard Moses <qalexeon@Polaris.lan>
2022-06-22 16:50:17 -0400

    Heading shows winner

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 
+//Random Generator
 let randomNumber1 = Math.ceil(Math.random()*6)
 let randomNumber2 = Math.ceil(Math.random()*6)
 
+//DOM Assignments
 let dice1 = document.querySelector('.img1')
 let dice2 = document.querySelector('.img2')
+let winH1 = document.querySelector('#h1')
 
+
+//Set Random Num to DOM Image
 dice1.setAttribute('src', `images/dice${randomNumber1}.png`)
 dice2.setAttribute('src', `images/dice${randomNumber2}.png`)
-
 
 console.log(dice1)
 console.log(dice1.getAttribute("src"))
 console.log(randomNumber1)
 
+winH1.innerHTML = 'Hola there';
+
+if (randomNumber1 > randomNumber2) {
+    winH1.innerHTML = 'Player 1 Wins!'
+} else if (randomNumber2 > randomNumber1) {
+    winH1.innerHTML = 'Player 2 Wins!'
+} else {
+    winH1.innerHTML = 'Draw!'
+}
